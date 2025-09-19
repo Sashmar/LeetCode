128e64b54d6d34f91152429bc9548a796dd69695
Sajin Satheesh Kumar <161288902+Sashmar@users.noreply.github.com>
2025-09-19 20:22:28 +0530

    Time: 2 ms (8.01%), Space: 8.4 MB (67.06%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
@@ -4,12 +4,12 @@ public:
         int num1 = 0; 
         int num2 = 0;
         for(int i = 1 ; i <= n ; i ++ ) {
-            if( i  % m != 0 ) {
+            if( i  % m == 0 ) {
                 num1 +=i;
             }
             else num2 += i;
         }
 
-        return num1 - num2 ;
+        return num2 - num1 ;
     }
 };
