817b88d6c5deaedc1c754273b01868233e44f010
Sajin Satheesh Kumar <161288902+Sashmar@users.noreply.github.com>
2025-10-08 01:58:36 +0530

    Time: 2 ms (77.56%), Space: 7.7 MB (63.65%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n ==0) return 0;
+        else if (n==1) return 1;
+        else {
+            int f = 0;
+            int s = 1;
+            int t;
+            int g;
+            int i = 0;
+            while (i < n-1) {
+                t = s +f;
+                f = s;
+                s= t;
+                i++;
+            }
+            return t;
+        }
+        return -1;
+    }
+};
