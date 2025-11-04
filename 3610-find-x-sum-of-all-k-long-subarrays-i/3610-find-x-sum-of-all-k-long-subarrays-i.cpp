f8b191cd4d20a872849a60ac1beced255ac372a8
Sajin Satheesh Kumar <161288902+Sashmar@users.noreply.github.com>
2025-11-04 09:06:45 +0530

    Time: 19 ms (43.77%), Space: 35.8 MB (44.09%) - LeetHub

---
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findXSum(vector<int>& nums, int k, int x) {
+        vector<int> answer;
+        int i = 0;
+        int b = 0;
+        while(i + k <= nums.size()) {
+            int a = i;
+            unordered_map<int,int> freq;
+            for(int j = a ; j < a+ k ; j ++) {
+                freq[nums[j]]++;
+            }
+            vector<pair<int, int>> v(freq.begin(), freq.end());
+            sort(v.begin(), v.end(), [](auto &a, auto &b) {
+                if (a.second == b.second) 
+                    return a.first > b.first;   // when freq equal, larger number first
+                return a.second > b.second;     // otherwise, higher freq first
+            });
+            int sum = 0;
+            for(int b = 0 ; b < x && b < v.size() ; b++){
+                sum += v[b].first * v[b].second;
+            }
+            answer.push_back(sum);
+            i++;
+        }
+        return answer;
+    }
+};
