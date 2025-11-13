8a6175075c5e8ee3c477747686f1f4d348af6b14
Sajin Satheesh Kumar <161288902+Sashmar@users.noreply.github.com>
2025-11-13 21:25:36 +0530

    Time: 596 ms (5.01%), Space: 152.5 MB (5.06%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,26 +2,29 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         if (s == " ") return 1;
-        int i = 0;
-        int c = 0;
-        int count =0;
-        unordered_set<char> st;
-        while(i < s.size()) {
-            if(st.contains(s[i])) {
-                i = i - count + 1;
-                c = (c > count) ? c : count;
-                st.clear();
-                count =0;
-            }
-            else {
-                st.insert(s[i]);
-                count++;
-                i++;
+        else {
+            int i = 0;
+            int c = 0;
+            int count =0;
+            unordered_set<char> st;
+            while(i < s.size()) {
+                if(st.contains(s[i])) {
+                    i = i - count + 1;
+                    c = (c > count) ? c : count;
+                    st.clear();
+                    count =0;
+                }
+                else {
+                    st.insert(s[i]);
+                    count++;
+                    i++;
+                }
+
             }
 
+            if(count > c ) return count;
+            else return c;
         }
-
-        if(count > c ) return count;
-        else return c;
+        return -1; 
     }
 };
