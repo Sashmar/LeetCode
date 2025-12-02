36f9f82149d8a681af1318b02b76a0ac6798928d
Sajin Satheesh Kumar <161288902+Sashmar@users.noreply.github.com>
2025-12-02 10:49:56 +0530

    Time: 4 ms (12.13%), Space: 8.8 MB (74.08%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.cpp
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -3,8 +3,7 @@ public:
     string getHint(string secret, string guess) {
         int b = 0;
         int c = 0;
-        int i = 0;
-        for(i = 0 ; i < secret.size();) {
+        for(int i = 0 ; i < secret.size();) {
             if(secret[i] == guess[i]) {
                 b++;
                 secret.erase(i, 1);
@@ -12,8 +11,7 @@ public:
             }
             else i++;
         }
-        i=0;
-        for(i = 0;i < secret.size(); i ++) {
+        for(int i = 0 ;i < secret.size(); i ++) {
             int pos = guess.find(secret[i]);
             if (pos != string :: npos) {
                 guess.erase(pos, 1);
