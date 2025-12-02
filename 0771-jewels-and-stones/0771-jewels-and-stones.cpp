e2404d5f5cd782b50df225adb92cd1261a691f4b
Sajin Satheesh Kumar <161288902+Sashmar@users.noreply.github.com>
2025-12-02 13:01:08 +0530

    Time: 0 ms (100%), Space: 8.2 MB (80.68%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        vector<char> a;
         int c =0;
-        for(int i = 0 ; i< jewels.size() ; i++) {
-            a.push_back(jewels[i]);
-        }
 
-        for(int i = 0 ; i< a.size() ; i++) {
+        for(int i = 0 ; i< jewels.size() ; i++) {
             for(int j = 0 ; j< stones.size() ; j++) {
-                if(a[i] == stones[j]) c++;
+                if(jewels[i] == stones[j]) c++;
             }
         }
 
