63be7565a1e6bdfaa97232ce968f9f36f882c4bc
Sajin Satheesh Kumar <161288902+Sashmar@users.noreply.github.com>
2026-05-12 20:44:11 +0530

    Time: 8 ms (5.89%), Space: 10.4 MB (14.79%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,8 +3,7 @@ public:
     bool isPalindrome(string s) {
         string a = "";
         for(int i = 0 ; i < s.size(); i ++) {
-            if(isalpha(s[i])) a += tolower(s[i]);
-            else if(isalnum(s[i])) a += s[i];
+            if(isalnum(s[i])) a += tolower(s[i]);
             else continue;
         }
         int i = 0;
