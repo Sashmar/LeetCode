321ce37d3125e0d6cff03c716e80b79aa36322d4
Sajin Satheesh Kumar <161288902+Sashmar@users.noreply.github.com>
2026-05-17 11:11:56 +0530

    Time: 0 ms (100%), Space: 34.8 MB (96.68%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        if(arr[0] == 1 && arr[1] == 1 && arr[2] == 1 && arr[3] == 1) return true;
+        int count = 0;
+        vector<int> b;
+        int n = arr.size();
+        b.push_back(start);
+        if(arr[start] == 0) return true;
+        while(count <= 40) {
+            vector<int> c;
+            for(int i = 0 ; i < b.size(); i ++) {
+                int curr = b[i];
+                if(arr[curr] < 0) continue;
+                int jump = arr[curr];
+                int g = (curr + jump);
+                if(g < n && arr[g] >= 0) {
+                    if(arr[g] == 0) return true;
+                    c.push_back(g);
+                }
+                int f = curr - jump;
+                if(f >= 0  && arr[f] >= 0) {
+                    if(arr[f] == 0) return true;
+                    c.push_back(f);
+                }
+                arr[curr] = -1;
+            }
+            b = move(c);
+            count++;
+        }
+
+        return false;
+    }
+};
