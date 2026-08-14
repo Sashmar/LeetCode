cfd789b6fce3672b1c5756ae21093282e6e8d4ca
Sajin Satheesh Kumar <161288902+Sashmar@users.noreply.github.com>
2026-08-14 11:37:26 +0530

    Time: 0 ms (100%), Space: 8.5 MB (48.3%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -4,15 +4,13 @@ public:
         vector<char> a;
         vector<char> b;
         for(int i = 0 ; i < s.size() ; i ++){
-            if(s[i] == '#') {
-                if (!a.empty()) a.pop_back();
-            }
+            if(!a.empty() && s[i] == '#') a.pop_back();
+            else if(s[i] == '#') continue;
             else a.push_back(s[i]);
         }
         for(int i = 0 ; i < t.size() ; i ++){
-            if (t[i] == '#') {
-                if (!b.empty()) b.pop_back();
-            }
+            if(!b.empty() && t[i] == '#') b.pop_back();
+            else if(t[i] == '#') continue;
             else b.push_back(t[i]);
         }
         if(a == b) return true;
