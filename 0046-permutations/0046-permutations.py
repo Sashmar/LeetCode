a7389f9b3c9d9c094775086b97f6d4c614ff846d
Sajin Satheesh Kumar <161288902+Sashmar@users.noreply.github.com>
2026-08-24 16:05:10 +0530

    Time: 0 ms (100%), Space: 12.6 MB (23.25%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        return list(itertools.permutations(nums))
+
+        
